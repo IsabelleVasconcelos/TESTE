3378fdc89bc3a35b0a85537b2e508b0c785f7793
IsabelleVasconcelos <88832627+IsabelleVasconcelos@users.noreply.github.com>
2021-10-06 15:50:29 -0300

    Add files via upload
    Pasta oficial, a outra pasta com as atividades foi perdida

---
+++ ATIVIDADES/AULA 4/aulaif.py
@@ -0,0 +1,13 @@
+''''
+aula 4 - if e else, mostrando como usar
+'''
+
+#variavelLogica = int(input('voce entendeu?')
+
+#if (variavelLogica)
+ #   print ("isabelle")
+#else 
+
+ #   print ("depois do if")
+
+#assistir a aula novamente
